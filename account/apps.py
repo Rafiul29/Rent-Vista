1c3f17c7d2676f58e87368061f0b86251a4ee78c
Rafiul29 <rafiulislam4472@gmail.com>
2024-07-06 13:56:09 +0600

    project initialized

--- account/apps.py
+++ account/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RentAdvertisementConfig(AppConfig):
+class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rent_advertisement'
+    name = 'account'
--- advertisement/apps.py
+++ advertisement/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RentRequestConfig(AppConfig):
+class AdvertisementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rent_request'
+    name = 'advertisement'

c1253fedffc1a167db4fefe52ae50bd9a910409b
Rafiul29 <rafiulislam4472@gmail.com>
2024-07-07 13:53:25 +0600

    favourite and review model create

--- advertisement/admin.py
+++ advertisement/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import RentAdvertisment
+from .models import RentAdvertisement,RentRequest,Favourite,Review
 # Register your models here.
 
 
-admin.site.register(RentAdvertisment)
+admin.site.register(RentAdvertisement)
+admin.site.register(RentRequest)
+admin.site.register(Favourite)
+admin.site.register(Review)
--- advertisement/models.py
+++ advertisement/models.py
@@ -3,7 +3,7 @@ from category.models import Category
 from account.models import User
 # Create your models here.
 
-class RentAdvertisment(models.Model):
+class RentAdvertisement(models.Model):
   title=models.CharField(max_length=100)
   description=models.TextField()
   price=models.DecimalField(max_digits=10,decimal_places=2)
@@ -15,3 +15,41 @@ class RentAdvertisment(models.Model):
 
   def __str__(self) -> str:
     return self.title
+  
+
+class RentRequest(models.Model):
+  advertisement=models.ForeignKey(RentAdvertisement,related_name='rent_requests', on_delete=models.CASCADE)
+  requester=models.ForeignKey(User,related_name='rent_requests', on_delete=models.CASCADE)
+  accepted = models.BooleanField(default=False)
+  created_at=models.DateTimeField(auto_now_add=True)
+  
+  def __str__(self) -> str:
+    return self.advertisement.title
+  
+
+class Favourite(models.Model):
+  user=models.ForeignKey(User,related_name='favorites',on_delete=models.CASCADE)
+  advertisement=models.ForeignKey(RentAdvertisement,related_name='favourites',on_delete=models.CASCADE)
+  created_at=models.DateTimeField(auto_now_add=True)
+
+  def __str__(self) -> str:
+    return self.user.first_name
+  
+STAR_CHOICES=[
+  ('⭐','⭐'),
+  ('⭐⭐','⭐⭐'),
+  ('⭐⭐⭐','⭐⭐⭐'),
+  ('⭐⭐⭐⭐','⭐⭐⭐⭐'),
+  ('⭐⭐⭐⭐⭐','⭐⭐⭐⭐⭐'),
+]
+
+
+class Review(models.Model):
+  advertisement=models.ForeignKey(RentAdvertisement,related_name='reviews',on_delete=models.CASCADE)
+  reviewer=models.ForeignKey(User,related_name="reviews",on_delete=models.CASCADE)
+  comment=models.TextField()
+  rating=models.CharField(choices=STAR_CHOICES,max_length=20)
+  created_at=models.DateTimeField(auto_now_add=True)
+
+  def __str__(self) -> str:
+    return self.advertisement.title
